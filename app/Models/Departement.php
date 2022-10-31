ce65c9a3ee239946e08f9cea5d0ec383ca9c6593
zahra rahouni <rahounizahra@gmail.com>
2022-10-31 15:55:36 +0100

    first commit

---
+++ app/Models/Departement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Departement extends Model
+{
+    protected $fillable = [
+        'nameD'      
+    ];
+    public function getEmployee()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
---
+++ app/Models/Employee.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'nameE',
+        'age',
+        'dept_id'       
+    ];
+    public function getDepartement()
+    {
+        return $this->belongsTo(Departement::class);
+    }
+}
+
